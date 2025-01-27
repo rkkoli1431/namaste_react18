33c72536af90706190059e38b5c11969a4fb190d
Rahul <rahulkoli9320@gmail.com>
2025-01-27 17:13:14 +0530

    restaurant-menu-page

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+const RestaurantMenu = ()=>{
+    const  [resInfo, setResInfo] = useState(null);
+    useEffect(()=>{
+        fetchMenu();
+    },[]);
+
+    const fetchMenu = async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=10576&catalog_qa=undefined&submitAction=ENTER");
+        const json = await data.json();
+        console.log(json);
+        setResInfo(json.data);
+    };
+    const {name,cuisines,costForTwoMessage} = resInfo?.cards[2].card?.card?.info;
+    return resInfo === null ? (
+    <Shimmer/>
+    ):(
+        <div>
+            <h1>{name} </h1>
+            <h2>{cuisines.join(",")}</h2>
+            <h3>{costForTwoMessage}</h3>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biryani</li>
+                <li>Pizza</li>
+                <li>Coffee</li>
+            </ul>
+        </div>
+    );
+};
+export default RestaurantMenu;
