60b4a164365cfdddd5c21e9ceffe89e0436de538
Rahul <rahulkoli9320@gmail.com>
2025-03-05 11:01:37 +0530

    about-

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,25 @@
 // import User from "./User";
-// import {Component} from "react";
-// import UserClass from "./UserClass";
+import {Component} from "react";
+import UserClass from "./UserClass";
 
 
-// class About extends Component{
-//   constructor(props){
-//     super(props);
-//   }
+class About extends Component{
+  constructor(props){
+    super(props);
+  }
 
-//   componentDidMount(){
-//    // console.log("Parent Component Did Mount");
-//   }
-//   render(){
-//   //  console.log("Parent Render");
-//     return (
-//       <div className="about">
-//         <UserClass name={"Rahul class"} location={"Boradi"}/>     
-//       </div>
-//     );
-//   }
-// }
+  componentDidMount(){
+   // console.log("Parent Component Did Mount");
+  }
+  render(){
+  //  console.log("Parent Render");
+    return (
+      <div className="about">
+        <UserClass name={"Rahul class"} location={"Boradi"}/>     
+      </div>
+    );
+  }
+}
 
 
-// export default About;
+export default About;
